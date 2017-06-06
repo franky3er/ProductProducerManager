e0791efde44a6872a60f7abfcd9e32834878c70d
franky3er <frankdreyer94@gmail.com>
2017-06-07 01:31:27 +0200

    feature/RequestAgent - completed initialize RequestAgents + add RequestAgents quantity

--- src/vs/productproducermanager/request/exception/RequestException.java
+++ src/vs/productproducermanager/request/exception/RequestException.java
@@ -1,8 +1,5 @@
 package vs.productproducermanager.request.exception;
 
-/**
- * Created by franky3er on 01.06.17.
- */
 public class RequestException extends Exception {
     public RequestException(Exception e) {
         super(e.getMessage(), e);
