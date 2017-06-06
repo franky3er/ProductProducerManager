10e15c5785db8dad0af88d14cd62bd004b970558
franky3er <frankdreyer94@gmail.com>
2017-06-06 22:34:55 +0200

    feature/ProductProducerFactory - complete finish initializing of ProductProducer + add neccessary config and printings

--- src/vs/productproducermanager/main/MainApplication.java
+++ src/vs/productproducermanager/main/MainApplication.java
@@ -2,7 +2,10 @@ package vs.productproducermanager.main;
 
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.json.simple.parser.ParseException;
 import vs.productproducermanager.mqtt.MqttClientSingleton;
+import vs.productproducermanager.producer.ProductProducer;
+import vs.productproducermanager.producer.ProductProducerFactory;
 
 import java.io.File;
 import java.io.FileReader;
@@ -14,9 +17,12 @@ public class MainApplication {
     private final static String PROJECT_CONFIG = System.getProperty("user.dir") + File.separator +
             "config" + File.separator + PROJECT_NAME + ".properties";
 
+    private final static String PRODUCTPRODUCERMANAGER_PRODUCTPRODUCER_CONFIGURATION_FILESOURCE =
+            "ProductProducerManager.ProductProducer.Configuration.fileSource";
     private final static String PRODUCTPRODUCERMANAGER_MQTT_IP = "ProductProducerManager.MQTT.IP";
     private final static String PRODUCTPRODUCERMANAGER_MQTT_PORT = "ProductProducerManager.MQTT.Port";
 
+    private static String productProducerConfigurationFileSource;
     private static String mqttIP;
     private static String mqttPort;
 
@@ -24,32 +30,50 @@ public class MainApplication {
     private static MqttClientSingleton client;
     private static MqttConnectOptions connOpts;
 
+    private static ProductProducer productProducer;
+
     public static void main(String [] args) {
         try {
             loadConfig();
             initialize();
             run();
         } catch (IOException e) {
+            System.err.println("ERROR : IOException");
             e.printStackTrace();
         } catch (MqttException e) {
+            System.err.println(String.format("ERROR : Connection to MQTTServer failed (IP: %s, Port: %s)",
+                    mqttIP, mqttPort));
+            e.printStackTrace();
+        } catch (ParseException e) {
+            System.err.println("ERROR : Parsing failed");
             e.printStackTrace();
         }
     }
 
     private static void loadConfig() throws IOException {
+        System.out.println("INFO : Load Config");
         Properties properties = new Properties();
         properties.load(new FileReader(PROJECT_CONFIG));
+        productProducerConfigurationFileSource =
+                properties.getProperty(PRODUCTPRODUCERMANAGER_PRODUCTPRODUCER_CONFIGURATION_FILESOURCE);
         mqttIP = properties.getProperty(PRODUCTPRODUCERMANAGER_MQTT_IP);
         mqttPort = properties.getProperty(PRODUCTPRODUCERMANAGER_MQTT_PORT);
     }
 
-    private static void initialize() throws MqttException {
+    private static void initialize() throws MqttException, IOException, ParseException {
+        initializeProductProducer();
         initializeMqttClient();
+    }
+
+    private static void initializeProductProducer() throws IOException, ParseException {
+        System.out.println(String.format("INFO : Create ProductProducer (FileSource: %s)",
+                productProducerConfigurationFileSource));
+        productProducer = ProductProducerFactory.create(productProducerConfigurationFileSource);
     }
 
     private static void initializeMqttClient() throws MqttException {
         persistence = new MemoryPersistence();
-        MqttClientSingleton.initialize(String.format("tcp://%s:%s", mqttIP, mqttPort), "TODO",
+        MqttClientSingleton.initialize(String.format("tcp://%s:%s", mqttIP, mqttPort), "bla",
                 persistence);
         client = MqttClientSingleton.getInstance();
         connOpts = new MqttConnectOptions();
@@ -73,6 +97,7 @@ public class MainApplication {
         });
 
         client.connect();
+        System.out.println(String.format("INFO : Connected to MQTTServer (IP: %s, Port: %s)", mqttIP, mqttPort));
     }
 
     private static void run() {
